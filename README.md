d391702d096043e5d45fb2962c435c653ae4624b
okr765 <me@okr765.com>
2024-05-03 00:57:23 -0400

    address rate limiting concerns

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # ThatFruedDued Kahoot Smasher
 
+A little script you can use to flood Kahoot games with numerous players that randomly answer questions. Fully functional as of **March 28, 2024**.
+
+## Usage
+
 This program was written using [Bun](https://bun.sh). However, it should also be fully compatible with comparable JavaScript runtimes such as [Deno](https://deno.land).
 
 To install dependencies:
--- index.ts
+++ index.ts
@@ -83,249 +83,251 @@ async function joinGame(pin: string, name: string) {
     headers: { cookie: `generated-uuid=${generatedUuid}` },
   });
 
-  await new Promise((res) => (ws.onopen = res));
+  ws.onopen = async () => {
+    let id = 1;
 
-  let id = 1;
+    const send = (message: any) => {
+      id++;
+      wsLogs && console.log("Sent", message);
+      ws.send(JSON.stringify(message));
+    };
 
-  const send = (message: any) => {
-    id++;
-    wsLogs && console.log("Sent", message);
-    ws.send(JSON.stringify(message));
-  };
-
-  ws.addEventListener("message", (event) => {
-    wsLogs && console.log("Received", JSON.parse(event.data as string));
-  });
-
-  const nextMessage = (condition?: (message: any) => boolean) =>
-    new Promise((res) => {
-      const listener = (event: any) => {
-        event = event as MessageEvent<string>;
-        const message = JSON.parse(event.data);
-        if (!condition || condition(message)) {
-          res(message);
-          ws.removeEventListener("message", listener);
-        }
-      };
-      ws.addEventListener("message", listener);
+    ws.addEventListener("message", (event) => {
+      wsLogs && console.log("Received", JSON.parse(event.data as string));
     });
 
-  send([
-    {
-      id: id.toString(),
-      version: "1.0",
-      minimumVersion: "1.0",
-      channel: "/meta/handshake",
-      supportedConnectionTypes: [
-        "websocket",
-        "long-polling",
-        "callback-polling",
-      ],
-      advice: { timeout: 60000, interval: 0 },
-      ext: { ack: true, timesync: { tc: Date.now(), l: 0, o: 0 } },
-    },
-  ]);
+    const nextMessage = (condition?: (message: any) => boolean) =>
+      new Promise((res) => {
+        const listener = (event: any) => {
+          event = event as MessageEvent<string>;
+          const message = JSON.parse(event.data);
+          if (!condition || condition(message)) {
+            res(message);
+            ws.removeEventListener("message", listener);
+          }
+        };
+        ws.addEventListener("message", listener);
+      });
 
-  const clientRes = (await nextMessage(
-    (message) => message?.[0]?.clientId
-  )) as [{ clientId: string }];
-  const { clientId } = clientRes[0];
+    send([
+      {
+        id: id.toString(),
+        version: "1.0",
+        minimumVersion: "1.0",
+        channel: "/meta/handshake",
+        supportedConnectionTypes: [
+          "websocket",
+          "long-polling",
+          "callback-polling",
+        ],
+        advice: { timeout: 60000, interval: 0 },
+        ext: { ack: true, timesync: { tc: Date.now(), l: 0, o: 0 } },
+      },
+    ]);
 
-  send([
-    {
-      id: id.toString(),
-      channel: "/meta/connect",
-      connectionType: "websocket",
-      advice: { timeout: 0 },
-      clientId,
-      ext: { ack: 0, timesync: { tc: Date.now(), l: 0, o: 0 } },
-    },
-  ]);
+    const clientRes = (await nextMessage(
+      (message) => message?.[0]?.clientId
+    )) as [{ clientId: string }];
+    const { clientId } = clientRes[0];
 
-  ws.addEventListener("message", async (event) => {
-    const data = event.data as string;
-    const message = JSON.parse(data);
+    send([
+      {
+        id: id.toString(),
+        channel: "/meta/connect",
+        connectionType: "websocket",
+        advice: { timeout: 0 },
+        clientId,
+        ext: { ack: 0, timesync: { tc: Date.now(), l: 0, o: 0 } },
+      },
+    ]);
 
-    if (message?.[0]?.channel === "/meta/connect") {
-      send([
-        {
-          id: id.toString(),
-          channel: "/meta/connect",
-          connectionType: "websocket",
-          clientId,
-          ext: {
-            ack: message[0].ext.ack,
-            // timesync: { tc: Date.now(), l: 0, o: 0 },
-          },
-        },
-      ]);
-    }
+    ws.addEventListener("message", async (event) => {
+      const data = event.data as string;
+      const message = JSON.parse(data);
 
-    if (
-      message?.[0]?.channel === "/service/player" &&
-      message[0].data.id === 2
-    ) {
-      await new Promise((r) => setTimeout(r, Math.floor(Math.random() * 3000)));
-      const content = JSON.parse(message[0].data.content);
-      if (content.type === "quiz") {
+      if (message?.[0]?.channel === "/meta/connect") {
         send([
           {
             id: id.toString(),
-            channel: "/service/controller",
-            data: {
-              gameid: pin,
-              type: "message",
-              host: "kahoot.it",
-              id: 45,
-              content: JSON.stringify({
-                type: "quiz",
-                choice: Math.floor(Math.random() * content.numberOfChoices),
-                questionIndex: content.gameBlockIndex,
-              }),
-            },
+            channel: "/meta/connect",
+            connectionType: "websocket",
             clientId,
-            ext: {},
-          },
-        ]);
-      } else if (content.type === "multiple_select_quiz") {
-        send([
-          {
-            id: id.toString(),
-            channel: "/service/controller",
-            data: {
-              gameid: pin,
-              type: "message",
-              host: "kahoot.it",
-              id: 45,
-              content: JSON.stringify({
-                type: "multiple_select_quiz",
-                choice: new Array(content.numberOfChoices)
-                  .fill(0)
-                  .map((_, i) => i)
-                  .filter(() => Math.random() < 0.5),
-                questionIndex: content.gameBlockIndex,
-              }),
+            ext: {
+              ack: message[0].ext.ack,
+              // timesync: { tc: Date.now(), l: 0, o: 0 },
             },
-            clientId,
-            ext: {},
-          },
-        ]);
-      } else if (content.type === "jumble") {
-        send([
-          {
-            id: id.toString(),
-            channel: "/service/controller",
-            data: {
-              gameid: pin,
-              type: "message",
-              host: "kahoot.it",
-              id: 45,
-              content: JSON.stringify({
-                type: "jumble",
-                choice: new Array(content.numberOfChoices)
-                  .fill(0)
-                  .map((_, i) => i)
-                  .sort(() => Math.random() - 0.5),
-                questionIndex: content.gameBlockIndex,
-              }),
-            },
-            clientId,
-            ext: {},
-          },
-        ]);
-      } else if (content.type === "open_ended") {
-        send([
-          {
-            id: id.toString(),
-            channel: "/service/controller",
-            data: {
-              gameid: pin,
-              type: "message",
-              host: "kahoot.it",
-              id: 45,
-              content: JSON.stringify({
-                type: "open_ended",
-                text: "amogus",
-                questionIndex: content.gameBlockIndex,
-              }),
-            },
-            clientId,
-            ext: {},
-          },
-        ]);
-      } else if (content.type === "slider") {
-        send([
-          {
-            id: id.toString(),
-            channel: "/service/controller",
-            data: {
-              gameid: pin,
-              type: "message",
-              host: "kahoot.it",
-              id: 45,
-              content: JSON.stringify({
-                type: "slider",
-                choice:
-                  Math.floor(
-                    (Math.random() *
-                      (content.maxRange - content.minRange + 1)) /
-                      content.step
-                  ) *
-                    content.step +
-                  content.minRange,
-                questionIndex: content.gameBlockIndex,
-              }),
-            },
-            clientId,
-            ext: {},
           },
         ]);
       }
-    }
-  });
 
-  await nextMessage();
+      if (
+        message?.[0]?.channel === "/service/player" &&
+        message[0].data.id === 2
+      ) {
+        await new Promise((r) =>
+          setTimeout(r, Math.floor(Math.random() * 3000))
+        );
+        const content = JSON.parse(message[0].data.content);
+        if (content.type === "quiz") {
+          send([
+            {
+              id: id.toString(),
+              channel: "/service/controller",
+              data: {
+                gameid: pin,
+                type: "message",
+                host: "kahoot.it",
+                id: 45,
+                content: JSON.stringify({
+                  type: "quiz",
+                  choice: Math.floor(Math.random() * content.numberOfChoices),
+                  questionIndex: content.gameBlockIndex,
+                }),
+              },
+              clientId,
+              ext: {},
+            },
+          ]);
+        } else if (content.type === "multiple_select_quiz") {
+          send([
+            {
+              id: id.toString(),
+              channel: "/service/controller",
+              data: {
+                gameid: pin,
+                type: "message",
+                host: "kahoot.it",
+                id: 45,
+                content: JSON.stringify({
+                  type: "multiple_select_quiz",
+                  choice: new Array(content.numberOfChoices)
+                    .fill(0)
+                    .map((_, i) => i)
+                    .filter(() => Math.random() < 0.5),
+                  questionIndex: content.gameBlockIndex,
+                }),
+              },
+              clientId,
+              ext: {},
+            },
+          ]);
+        } else if (content.type === "jumble") {
+          send([
+            {
+              id: id.toString(),
+              channel: "/service/controller",
+              data: {
+                gameid: pin,
+                type: "message",
+                host: "kahoot.it",
+                id: 45,
+                content: JSON.stringify({
+                  type: "jumble",
+                  choice: new Array(content.numberOfChoices)
+                    .fill(0)
+                    .map((_, i) => i)
+                    .sort(() => Math.random() - 0.5),
+                  questionIndex: content.gameBlockIndex,
+                }),
+              },
+              clientId,
+              ext: {},
+            },
+          ]);
+        } else if (content.type === "open_ended") {
+          send([
+            {
+              id: id.toString(),
+              channel: "/service/controller",
+              data: {
+                gameid: pin,
+                type: "message",
+                host: "kahoot.it",
+                id: 45,
+                content: JSON.stringify({
+                  type: "open_ended",
+                  text: "amogus",
+                  questionIndex: content.gameBlockIndex,
+                }),
+              },
+              clientId,
+              ext: {},
+            },
+          ]);
+        } else if (content.type === "slider") {
+          send([
+            {
+              id: id.toString(),
+              channel: "/service/controller",
+              data: {
+                gameid: pin,
+                type: "message",
+                host: "kahoot.it",
+                id: 45,
+                content: JSON.stringify({
+                  type: "slider",
+                  choice:
+                    Math.floor(
+                      (Math.random() *
+                        (content.maxRange - content.minRange + 1)) /
+                        content.step
+                    ) *
+                      content.step +
+                    content.minRange,
+                  questionIndex: content.gameBlockIndex,
+                }),
+              },
+              clientId,
+              ext: {},
+            },
+          ]);
+        }
+      }
+    });
 
-  const loginId = id.toString();
-  send([
-    {
-      id: loginId,
-      channel: "/service/controller",
-      data: {
-        type: "login",
-        gameid: pin,
-        host: "kahoot.it",
-        name,
-        content: JSON.stringify({
-          device: {
-            userAgent:
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.3",
-          },
-          screen: { width: 1920, height: 1080 },
-        }),
+    await nextMessage();
+
+    const loginId = id.toString();
+    send([
+      {
+        id: loginId,
+        channel: "/service/controller",
+        data: {
+          type: "login",
+          gameid: pin,
+          host: "kahoot.it",
+          name,
+          content: JSON.stringify({
+            device: {
+              userAgent:
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.3",
+            },
+            screen: { width: 1920, height: 1080 },
+          }),
+        },
+        clientId,
+        ext: {},
       },
-      clientId,
-      ext: {},
-    },
-  ]);
+    ]);
 
-  await nextMessage((message) => message?.[0]?.id === loginId);
+    await nextMessage((message) => message?.[0]?.id === loginId);
 
-  send([
-    {
-      id: id.toString(),
-      channel: "/service/controller",
-      data: {
-        gameid: pin,
-        type: "message",
-        host: "kahoot.it",
-        id: 16,
-        content: JSON.stringify({ usingNamerator: false }),
+    send([
+      {
+        id: id.toString(),
+        channel: "/service/controller",
+        data: {
+          gameid: pin,
+          type: "message",
+          host: "kahoot.it",
+          id: 16,
+          content: JSON.stringify({ usingNamerator: false }),
+        },
+        clientId,
+        ext: {},
       },
-      clientId,
-      ext: {},
-    },
-  ]);
+    ]);
+  };
 }
 
 const pin = prompt("Game PIN:")!;
@@ -338,9 +340,13 @@ if (isNaN(bots)) {
 
 for (let i = 0; i < bots; i++) {
   const name = faker.person.firstName();
-  joinGame(pin, name).catch((e) => {
-    console.error(e);
-  });
   console.log("Joining as", name);
-  await new Promise((r) => setTimeout(r, 1000 / 15));
+  await Promise.allSettled([
+    joinGame(pin, name)
+      .then(() => console.log("Joined as", name))
+      .catch((e) => {
+        console.error(e);
+      }),
+    new Promise((r) => setTimeout(r, 1000 / 15)),
+  ]);
 }
